3072f77b61622c0dd943d389719bb6f444aabb38
Greem4 <i@greem4.ru>
2024-12-05 15:23:32 +0300

    init project

---
+++ src/main/java/com/greem4/springmedicines/SpringMedicinesApplication.java
@@ -0,0 +1,13 @@
+package com.greem4.springmedicines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMedicinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMedicinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greem4/springmedicines/SpringMedicinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greem4.springmedicines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMedicinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
