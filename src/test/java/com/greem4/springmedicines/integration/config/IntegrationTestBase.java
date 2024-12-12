145931b2c1254ec091864119fb2250a82da0f093
Greem4 <i@greem4.ru>
2024-12-12 22:07:00 +0300

    add aspect

--- src/test/java/com/greem4/springmedicines/integration/config/IntegrationTestBase.java
+++ src/test/java/com/greem4/springmedicines/integration/config/IntegrationTestBase.java
@@ -1,18 +1,23 @@
 package com.greem4.springmedicines.integration.config;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 @Sql({
         "classpath:sql/data.sql"
 })
 @Testcontainers
+@ActiveProfiles("test")
 public abstract class IntegrationTestBase {
 
     @Container
