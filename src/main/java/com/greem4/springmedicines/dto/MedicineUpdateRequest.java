65e9c57622d0817542f418f5da88e10e4d0dc95e
Greem4 <i@greem4.ru>
2025-01-11 12:35:06 +0300

    update time format

--- src/main/java/com/greem4/springmedicines/dto/MedicineUpdateRequest.java
+++ src/main/java/com/greem4/springmedicines/dto/MedicineUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.greem4.springmedicines.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -8,6 +9,7 @@ import java.time.LocalDate;
 public record MedicineUpdateRequest(
         @NotBlank() String name,
         @NotBlank() String serialNumber,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
         @NotNull() LocalDate expirationDate
 ) {
 }
